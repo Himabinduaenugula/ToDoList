b72368858fa7b8e7cfa17f7e5f34760a211e645d
Himabindu9805 <himabindu9805@gmail.com>
2024-05-29 14:47:31 -0400

    Setup

---
+++ src/components/ToDoList/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './ToDoList/T';
+
+const ToDoList = () => {
+    return (
+        <div className="to-do-header__text">
+            <span>Welcome</span>
+        </div>
+    )
+}
+
+export default ToDoList;
